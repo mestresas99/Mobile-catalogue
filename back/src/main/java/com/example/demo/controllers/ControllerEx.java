e2cf1e8d224c2ac85ed37c2f7f57f73c4b9557a4
PABLO MESTRE QUÍLEZ <100383692@alumnos.uc3m.es>
2021-10-29 19:51:03 +0200

    9 Mobiles added

--- back/src/main/java/com/example/demo/controllers/ControllerEx.java
+++ back/src/main/java/com/example/demo/controllers/ControllerEx.java
@@ -24,8 +24,16 @@ public class ControllerEx {
 	@PostConstruct
 	public void init() {
 		Set<Mobile> mobile = new HashSet<>();
-		mobile.add(new Mobile(1, "iPhone 7", "Apple","lorem ipsum dolor sit amet consectetur", "black", 769, "IPhone_7.png", "4.7 inch IPS", "A10 fusion", 2));
+		mobile.add(new Mobile(1, "iPhone 7", "Apple","lorem ipsum dolor sit amet consectetur", "black", 600, "IPhone_7.png", "4.7 inch IPS", "A10 fusion", 2));
 		mobile.add(new Mobile(2, "iPhone 10", "Apple","lorem ipsum dolor sit amet consectetur", "white", 930, "IPhone_10.png", "13 inch IPS", "A10 fusion", 4));
+		mobile.add(new Mobile(1, "Samsung s20", "Apple","lorem ipsum dolor sit amet consectetur", "black", 820, "Samsung_s20.png", "4.7 inch IPS", "A14 Bionic", 2));
+		mobile.add(new Mobile(1, "iPhone 11 Pro", "Apple","lorem ipsum dolor sit amet consectetur", "black", 900, "IPhone_11.png", "7.5 inch IPS", "A14 Bionic", 2));
+		mobile.add(new Mobile(1, "iPhone 12", "Apple","lorem ipsum dolor sit amet consectetur", "white", 850, "IPhone_12.png", "4.7 inch IPS", "A14 Bionic", 4));
+		mobile.add(new Mobile(1, "Huawei nova 8i", "Huawei","lorem ipsum dolor sit amet consectetur", "black", 550, "Huawei_8.png", "6.57 inch IPS", "Snapdragon 778G", 8));
+		mobile.add(new Mobile(1, "Huawei nova 9", "Huawei","lorem ipsum dolor sit amet consectetur", "black", 689, "Huawei_9.png", "6.57 inch IPS", "Snapdragon 778G fusion", 8));
+		mobile.add(new Mobile(1, "Samsung s10", "Samsung","lorem ipsum dolor sit amet consectetur", "black", 700, "Samsung_s10.png", "4.5 inch IPS", "Octa-core", 12));
+		mobile.add(new Mobile(1, "Samsung Note 20 Ultra", "Samsung","lorem ipsum dolor sit amet consectetur", "green", 749, "Samsung_note.png", "6.7 inch IPS", "Octa-core", 12));
+		
 		
 		user = new User(mobile);
 		
