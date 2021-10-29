778158d7af459a334342c448737faef1dd032025
PABLO MESTRE QUÍLEZ <100383692@alumnos.uc3m.es>
2021-10-29 03:03:48 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, {Component} from 'react'
-import {mobile} from './object/mobile.js';
+import phone from './components/phone.js';
 import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <div>
-        <h2>List of mobiles:</h2>
-        <div class="list">Samsung</div>
-      </div>
+        <div>
+          <h1>List of available mobiles:</h1> 
+          <div class='mobiles'>
+            <div class='mobile1'>
+            Samsung s10
+            </div>
+            <div class='mobile2'>
+            Iphone 12 
+            </div>
+            <div class='mobile3'>
+            Iphone 12 
+            </div>
+            <div class='mobile4'>
+            Samsung s3 
+            </div>
+            <div class='mobile5'>
+            Iphone 10 
+            </div>
+            <div class='mobile6'>
+            Huawei mate 
+            </div>
+            <div class='mobile7'>
+            Samsung s10
+            </div>
+            <div class='mobile8'>
+            Iphone 13
+            </div>
+            <div class='mobile9'>
+            Samsung s note
+            </div>
+
+          </div>
+        </div>
     );
   }
 }
---
+++ src/components/phone.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './phone.css'
+
+export function phone(name) {
+        return (
+            <ul><li>{name}</li> </ul>
+        )
+}
+
+export default phone;
