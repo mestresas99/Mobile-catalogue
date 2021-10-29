48dbb2622d5f8070d193b8a4bbaf14b743857606
PABLO MESTRE QUÍLEZ <100383692@alumnos.uc3m.es>
2021-10-29 19:57:27 +0200

    front

--- src/App.js
+++ src/App.js
@@ -7,36 +7,7 @@ class App extends Component {
     return (
         <div>
           <h1>List of available mobiles:</h1> 
-          <div class='mobiles'>
-            <div class='mobile1'>
-            Samsung s10
-            </div>
-            <div class='mobile2'>
-            Iphone 12 
-            </div>
-            <div class='mobile3'>
-            Iphone 12 
-            </div>
-            <div class='mobile4'>
-            Samsung s3 
-            </div>
-            <div class='mobile5'>
-            Iphone 10 
-            </div>
-            <div class='mobile6'>
-            Huawei mate 
-            </div>
-            <div class='mobile7'>
-            Samsung s10
-            </div>
-            <div class='mobile8'>
-            Iphone 13
-            </div>
-            <div class='mobile9'>
-            Samsung s note
-            </div>
-
-          </div>
+        
         </div>
     );
   }
