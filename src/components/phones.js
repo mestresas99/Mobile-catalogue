194ace0c56609b734e8e87682a659f863a4845af
PABLO MESTRE QUÍLEZ <100383692@alumnos.uc3m.es>
2021-10-30 19:45:16 +0200

    images

--- src/components/phones.js
+++ src/components/phones.js
@@ -1,5 +1,14 @@
 import React from "react";
 import './phones.css'
+import a from './images/450_1000.jpeg';
+import b from './images/235548_ed97333e-0c9d-44bc-8076-3e98c00b59ac.jpg';
+import c from './images/a4012e4f-8fae-45a5-88b0-2ecec9b2d2e3_1.18a6f3642dab5336c94ebae20262d64b.jpeg';
+import d from './images/gris1.jpg';
+import e from './images/Huawei-Smartphone-Nova-8i-Starry-Black-(HMS)-9.jpg';
+import f from './images/iphone-12-white-select-2020.jpeg';
+import g from './images/iPhone-X-64GB-Silver-1-1-1-1.png.jpeg';
+import h from './images/picsart_09-04-02-35-341-b3c1ca0e5aaccbf09415998581258380-640-0.jpg';
+import i from './images/Samsung-Galaxy-Note-20-verde.jpg.webp';
 
 
 const Mobile = ({mobiles}) => {
