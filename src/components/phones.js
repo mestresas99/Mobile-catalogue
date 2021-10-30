a5dc25ef40538ed21f9e6a38a87a66e963d99331
PABLO MESTRE QUÍLEZ <100383692@alumnos.uc3m.es>
2021-10-30 20:16:09 +0200

    image update

--- src/components/phones.js
+++ src/components/phones.js
@@ -17,15 +17,41 @@ const Mobile = ({mobiles}) => {
           <h1>List of available phones:</h1>
           {mobiles.slice(0,1).map((x) => (
                   <div key={x.id}>
-                      <img src={x.imageFile} alt={x.name} width="200" height ="133"/>  
+                    <p>{(() => {switch (x.imageFile) {
+                                case "./images/450_1000.jpeg": return <img src={a} alt={x.name} width="200" height ="133"/>;
+                                case "./images/235548_ed97333e-0c9d-44bc-8076-3e98c00b59ac.jpg": return <img src={b} alt={x.name} width="200" height ="133"/>;
+                                case "./images/a4012e4f-8fae-45a5-88b0-2ecec9b2d2e3_1.18a6f3642dab5336c94ebae20262d64b.jpeg": return <img src={c} alt={x.name} width="200" height ="133"/>;
+                                case "./images/gris1.jpg": return <img src={d} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Huawei-Smartphone-Nova-8i-Starry-Black-(HMS)-9.jpg": return <img src={e} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iphone-12-white-select-2020.jpeg": return <img src={f} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iPhone-X-64GB-Silver-1-1-1-1.png.jpeg": return <img src={g} alt={x.name} width="200" height ="133"/>;
+                                case "./images/picsart_09-04-02-35-341-b3c1ca0e5aaccbf09415998581258380-640-0.jpg": return <img src={h} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Samsung-Galaxy-Note-20-verde.jpg.webp": return <img src={i} alt={x.name} width="200" height ="133"/>;
+                                default: return <img src={x.imageFile} alt={x.name} width="200" height ="133"/>;
+                            }
+                    })()}
+                    </p>
                       <div class="name1"><h2>{x.name} - {x.manufacturer}</h2></div>
                       <div class="price1"><h2>price: {x.price}$</h2></div>
                       <button class="button1">info</button>
                   </div>
           ))}
           {mobiles.slice(1,2).map((x) => (
-                  <div key={x.id}>
-                      <img src={x.imageFile} alt={x.name} width="200" height ="133"/>   
+                  <div key={x.id}>  
+                  <p>{(() => {switch (x.imageFile) {
+                                case "./images/450_1000.jpeg": return <img src={a} alt={x.name} width="200" height ="133"/>;
+                                case "./images/235548_ed97333e-0c9d-44bc-8076-3e98c00b59ac.jpg": return <img src={b} alt={x.name} width="200" height ="133"/>;
+                                case "./images/a4012e4f-8fae-45a5-88b0-2ecec9b2d2e3_1.18a6f3642dab5336c94ebae20262d64b.jpeg": return <img src={c} alt={x.name} width="200" height ="133"/>;
+                                case "./images/gris1.jpg": return <img src={d} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Huawei-Smartphone-Nova-8i-Starry-Black-(HMS)-9.jpg": return <img src={e} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iphone-12-white-select-2020.jpeg": return <img src={f} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iPhone-X-64GB-Silver-1-1-1-1.png.jpeg": return <img src={g} alt={x.name} width="200" height ="133"/>;
+                                case "./images/picsart_09-04-02-35-341-b3c1ca0e5aaccbf09415998581258380-640-0.jpg": return <img src={h} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Samsung-Galaxy-Note-20-verde.jpg.webp": return <img src={i} alt={x.name} width="200" height ="133"/>;
+                                default: return <img src={x.imageFile} alt={x.name} width="200" height ="133"/>;
+                            }
+                    })()}
+                    </p>
                       <div class="name2"><h2>{x.name} - {x.manufacturer}</h2></div>
                       <div class="price2"><h2>price: {x.price}$</h2></div>
                       <button class="button2">info</button>
@@ -33,7 +59,20 @@ const Mobile = ({mobiles}) => {
           ))}
           {mobiles.slice(2,3).map((x) => (
                   <div key={x.id}>
-                      <img src={x.imageFile} alt={x.name} width="200" height ="133"/> 
+                    <p>{(() => {switch (x.imageFile) {
+                                case "./images/450_1000.jpeg": return <img src={a} alt={x.name} width="200" height ="133"/>;
+                                case "./images/235548_ed97333e-0c9d-44bc-8076-3e98c00b59ac.jpg": return <img src={b} alt={x.name} width="200" height ="133"/>;
+                                case "./images/a4012e4f-8fae-45a5-88b0-2ecec9b2d2e3_1.18a6f3642dab5336c94ebae20262d64b.jpeg": return <img src={c} alt={x.name} width="200" height ="133"/>;
+                                case "./images/gris1.jpg": return <img src={d} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Huawei-Smartphone-Nova-8i-Starry-Black-(HMS)-9.jpg": return <img src={e} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iphone-12-white-select-2020.jpeg": return <img src={f} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iPhone-X-64GB-Silver-1-1-1-1.png.jpeg": return <img src={g} alt={x.name} width="200" height ="133"/>;
+                                case "./images/picsart_09-04-02-35-341-b3c1ca0e5aaccbf09415998581258380-640-0.jpg": return <img src={h} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Samsung-Galaxy-Note-20-verde.jpg.webp": return <img src={i} alt={x.name} width="200" height ="133"/>;
+                                default: return <img src={x.imageFile} alt={x.name} width="200" height ="133"/>;
+                            }
+                    })()}
+                    </p>
                       <div class="name3"><h2>{x.name} - {x.manufacturer}</h2></div>
                       <div class="price3"><h2>price: {x.price}$</h2></div>
                       <button class="button3">info</button>
@@ -41,7 +80,20 @@ const Mobile = ({mobiles}) => {
           ))}
           {mobiles.slice(3,4).map((x) => (
                   <div key={x.id}>
-                      <img src={x.imageFile} alt={x.name} width="200" height ="133"/> 
+                      <p>{(() => {switch (x.imageFile) {
+                                case "./images/450_1000.jpeg": return <img src={a} alt={x.name} width="200" height ="133"/>;
+                                case "./images/235548_ed97333e-0c9d-44bc-8076-3e98c00b59ac.jpg": return <img src={b} alt={x.name} width="200" height ="133"/>;
+                                case "./images/a4012e4f-8fae-45a5-88b0-2ecec9b2d2e3_1.18a6f3642dab5336c94ebae20262d64b.jpeg": return <img src={c} alt={x.name} width="200" height ="133"/>;
+                                case "./images/gris1.jpg": return <img src={d} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Huawei-Smartphone-Nova-8i-Starry-Black-(HMS)-9.jpg": return <img src={e} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iphone-12-white-select-2020.jpeg": return <img src={f} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iPhone-X-64GB-Silver-1-1-1-1.png.jpeg": return <img src={g} alt={x.name} width="200" height ="133"/>;
+                                case "./images/picsart_09-04-02-35-341-b3c1ca0e5aaccbf09415998581258380-640-0.jpg": return <img src={h} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Samsung-Galaxy-Note-20-verde.jpg.webp": return <img src={i} alt={x.name} width="200" height ="133"/>;
+                                default: return <img src={x.imageFile} alt={x.name} width="200" height ="133"/>;
+                            }
+                    })()}
+                    </p>
                       <div class="name4"><h2>{x.name} - {x.manufacturer}</h2></div>
                       <div class="price4"><h2>price: {x.price}$</h2></div>
                       <button class="button4">info</button>
@@ -49,7 +101,20 @@ const Mobile = ({mobiles}) => {
           ))}
           {mobiles.slice(4,5).map((x) => (
                   <div key={x.id}>
-                      <img src={x.imageFile} alt={x.name} width="200" height ="133"/> 
+                      <p>{(() => {switch (x.imageFile) {
+                                case "./images/450_1000.jpeg": return <img src={a} alt={x.name} width="200" height ="133"/>;
+                                case "./images/235548_ed97333e-0c9d-44bc-8076-3e98c00b59ac.jpg": return <img src={b} alt={x.name} width="200" height ="133"/>;
+                                case "./images/a4012e4f-8fae-45a5-88b0-2ecec9b2d2e3_1.18a6f3642dab5336c94ebae20262d64b.jpeg": return <img src={c} alt={x.name} width="200" height ="133"/>;
+                                case "./images/gris1.jpg": return <img src={d} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Huawei-Smartphone-Nova-8i-Starry-Black-(HMS)-9.jpg": return <img src={e} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iphone-12-white-select-2020.jpeg": return <img src={f} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iPhone-X-64GB-Silver-1-1-1-1.png.jpeg": return <img src={g} alt={x.name} width="200" height ="133"/>;
+                                case "./images/picsart_09-04-02-35-341-b3c1ca0e5aaccbf09415998581258380-640-0.jpg": return <img src={h} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Samsung-Galaxy-Note-20-verde.jpg.webp": return <img src={i} alt={x.name} width="200" height ="133"/>;
+                                default: return <img src={x.imageFile} alt={x.name} width="200" height ="133"/>;
+                            }
+                    })()}
+                    </p>
                       <div class="name5"><h2>{x.name} - {x.manufacturer}</h2></div>
                       <div class="price5"><h2>price: {x.price}$</h2></div>
                       <button class="button5">info</button>
@@ -57,7 +122,20 @@ const Mobile = ({mobiles}) => {
           ))}
           {mobiles.slice(5,6).map((x) => (
                   <div key={x.id}>
-                      <img src={x.imageFile} alt={x.name} width="200" height ="133"/> 
+                      <p>{(() => {switch (x.imageFile) {
+                                case "./images/450_1000.jpeg": return <img src={a} alt={x.name} width="200" height ="133"/>;
+                                case "./images/235548_ed97333e-0c9d-44bc-8076-3e98c00b59ac.jpg": return <img src={b} alt={x.name} width="200" height ="133"/>;
+                                case "./images/a4012e4f-8fae-45a5-88b0-2ecec9b2d2e3_1.18a6f3642dab5336c94ebae20262d64b.jpeg": return <img src={c} alt={x.name} width="200" height ="133"/>;
+                                case "./images/gris1.jpg": return <img src={d} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Huawei-Smartphone-Nova-8i-Starry-Black-(HMS)-9.jpg": return <img src={e} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iphone-12-white-select-2020.jpeg": return <img src={f} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iPhone-X-64GB-Silver-1-1-1-1.png.jpeg": return <img src={g} alt={x.name} width="200" height ="133"/>;
+                                case "./images/picsart_09-04-02-35-341-b3c1ca0e5aaccbf09415998581258380-640-0.jpg": return <img src={h} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Samsung-Galaxy-Note-20-verde.jpg.webp": return <img src={i} alt={x.name} width="200" height ="133"/>;
+                                default: return <img src={x.imageFile} alt={x.name} width="200" height ="133"/>;
+                            }
+                    })()}
+                    </p>
                       <div class="name6"><h2>{x.name} - {x.manufacturer}</h2></div>
                       <div class="price6"><h2>price: {x.price}$</h2></div>
                       <button class="button6">info</button>
@@ -65,7 +143,20 @@ const Mobile = ({mobiles}) => {
           ))}
            {mobiles.slice(6,7).map((x) => (
                   <div key={x.id}>
-                      <img src={x.imageFile} alt={x.name} width="200" height ="133"/> 
+                      <p>{(() => {switch (x.imageFile) {
+                                case "./images/450_1000.jpeg": return <img src={a} alt={x.name} width="200" height ="133"/>;
+                                case "./images/235548_ed97333e-0c9d-44bc-8076-3e98c00b59ac.jpg": return <img src={b} alt={x.name} width="200" height ="133"/>;
+                                case "./images/a4012e4f-8fae-45a5-88b0-2ecec9b2d2e3_1.18a6f3642dab5336c94ebae20262d64b.jpeg": return <img src={c} alt={x.name} width="200" height ="133"/>;
+                                case "./images/gris1.jpg": return <img src={d} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Huawei-Smartphone-Nova-8i-Starry-Black-(HMS)-9.jpg": return <img src={e} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iphone-12-white-select-2020.jpeg": return <img src={f} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iPhone-X-64GB-Silver-1-1-1-1.png.jpeg": return <img src={g} alt={x.name} width="200" height ="133"/>;
+                                case "./images/picsart_09-04-02-35-341-b3c1ca0e5aaccbf09415998581258380-640-0.jpg": return <img src={h} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Samsung-Galaxy-Note-20-verde.jpg.webp": return <img src={i} alt={x.name} width="200" height ="133"/>;
+                                default: return <img src={x.imageFile} alt={x.name} width="200" height ="133"/>;
+                            }
+                    })()}
+                    </p>
                       <div class="name7"><h2>{x.name} - {x.manufacturer}</h2></div>
                       <div class="price7"><h2>price: {x.price}$</h2></div>
                       <button class="button7">info</button>
@@ -73,7 +164,20 @@ const Mobile = ({mobiles}) => {
           ))}
           {mobiles.slice(7,8).map((x) => (
                   <div key={x.id}>
-                      <img src={x.imageFile} alt={x.name} width="200" height ="133"/> 
+                      <p>{(() => {switch (x.imageFile) {
+                                case "./images/450_1000.jpeg": return <img src={a} alt={x.name} width="200" height ="133"/>;
+                                case "./images/235548_ed97333e-0c9d-44bc-8076-3e98c00b59ac.jpg": return <img src={b} alt={x.name} width="200" height ="133"/>;
+                                case "./images/a4012e4f-8fae-45a5-88b0-2ecec9b2d2e3_1.18a6f3642dab5336c94ebae20262d64b.jpeg": return <img src={c} alt={x.name} width="200" height ="133"/>;
+                                case "./images/gris1.jpg": return <img src={d} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Huawei-Smartphone-Nova-8i-Starry-Black-(HMS)-9.jpg": return <img src={e} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iphone-12-white-select-2020.jpeg": return <img src={f} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iPhone-X-64GB-Silver-1-1-1-1.png.jpeg": return <img src={g} alt={x.name} width="200" height ="133"/>;
+                                case "./images/picsart_09-04-02-35-341-b3c1ca0e5aaccbf09415998581258380-640-0.jpg": return <img src={h} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Samsung-Galaxy-Note-20-verde.jpg.webp": return <img src={i} alt={x.name} width="200" height ="133"/>;
+                                default: return <img src={x.imageFile} alt={x.name} width="200" height ="133"/>;
+                            }
+                    })()}
+                    </p>
                       <div class="name8"><h2>{x.name} - {x.manufacturer}</h2></div>
                       <div class="price8"><h2>price: {x.price}$</h2></div>
                       <button class="button8">info</button>
@@ -81,7 +185,20 @@ const Mobile = ({mobiles}) => {
           ))}
           {mobiles.slice(8,9).map((x) => (
                   <div key={x.id}>
-                      <img src={x.imageFile} alt={x.name} width="200" height ="133"/> 
+                      <p>{(() => {switch (x.imageFile) {
+                                case "./images/450_1000.jpeg": return <img src={a} alt={x.name} width="200" height ="133"/>;
+                                case "./images/235548_ed97333e-0c9d-44bc-8076-3e98c00b59ac.jpg": return <img src={b} alt={x.name} width="200" height ="133"/>;
+                                case "./images/a4012e4f-8fae-45a5-88b0-2ecec9b2d2e3_1.18a6f3642dab5336c94ebae20262d64b.jpeg": return <img src={c} alt={x.name} width="200" height ="133"/>;
+                                case "./images/gris1.jpg": return <img src={d} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Huawei-Smartphone-Nova-8i-Starry-Black-(HMS)-9.jpg": return <img src={e} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iphone-12-white-select-2020.jpeg": return <img src={f} alt={x.name} width="200" height ="133"/>;
+                                case "./images/iPhone-X-64GB-Silver-1-1-1-1.png.jpeg": return <img src={g} alt={x.name} width="200" height ="133"/>;
+                                case "./images/picsart_09-04-02-35-341-b3c1ca0e5aaccbf09415998581258380-640-0.jpg": return <img src={h} alt={x.name} width="200" height ="133"/>;
+                                case "./images/Samsung-Galaxy-Note-20-verde.jpg.webp": return <img src={i} alt={x.name} width="200" height ="133"/>;
+                                default: return <img src={x.imageFile} alt={x.name} width="200" height ="133"/>;
+                            }
+                    })()}
+                    </p>
                       <div class="name9"><h2>{x.name} - {x.manufacturer}</h2></div>
                       <div class="price9"><h2>price: {x.price}$</h2></div>
                       <button class="button9">info</button>
